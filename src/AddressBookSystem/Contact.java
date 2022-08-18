1a4559f351fc7866790fba84d6efc7799e1ed261
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-18 20:09:25 +0530

    [Saravanan]: Added program to creation of contact

---
+++ src/AddressBookSystem/Contact.java
@@ -0,0 +1,15 @@
+package AddressBookSystem;
+
+public class Contact {
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	long phoneNumber;
+	String email;
+	
+	
+
+}
